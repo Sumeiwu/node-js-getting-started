e9b147b1f205d838a4998579dd6725ce597d90aa
sumeiwu <sumeiwu@s-MacBook-Pro.attlocal.net>
2019-10-01 00:50:15 -0700

    Add cool face API

--- index.js
+++ index.js
@@ -12,29 +12,6 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-
-
-  .get('/mongodb', function (request, response) {
-                             
-     mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
-         if(err) throw err;
-          //get collection of routes
-          var Routes = db.collection('Routes');
-          //get all Routes with frequency >=1
-          Routes.find({ frequency : { $gte: 0 } }).sort({ name: 1 }).toArray(function (err, docs) {
-              if(err) throw err;
-                               
-              response.render('pages/mongodb', {results: docs});
-   
-          });
-   
-           //close connection when your app is terminating.
-            db.close(function (err) {
-                     if(err) throw err;
-             });
- });//end of connect
-});//end app.get
-
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
@@ -61,3 +38,4 @@ showTimes = () => {
   }
   return result;
 }
+
