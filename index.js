eb4969825c3cda4921aebf77bb4a1a1e1fd31f34
sumeiwu <sumeiwu@s-MacBook-Pro.attlocal.net>
2019-09-30 23:44:50 -0700

    commit for deploy to heroku

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
@@ -27,33 +27,33 @@ express()
       res.send("Error " + err);
     }
   })
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-  showTimes = () => {
-    let result = ''
-    const times = process.env.TIMES || 5
-    for (i = 0; i < times; i++) {
-      result += i + ' '
-    }
-    return result;
+showTimes = () => {
+  let result = ''
+  const times = process.env.TIMES || 5
+  for (i = 0; i < times; i++) {
+    result += i + ' '
   }
+  return result;
+}
 
-
+//PUT INSIDE YOUR index.js file (or whereever)
+// Assumes DATABASE_URL in the heroku configs points to your PostGres database
+// setups the url /db to connect to database and does a query from a table test_table
 var pg = require('pg');
     app.get('/db', function (request, response) {
-      pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-        client.query('SELECT * FROM test_table', function (err, result) {
-          done();
-          if (err)
-          { console.error(err); response.send("Error" + err);}
-          else
-          {response.render('pages/db', {results:result.rows});}
-          
-        });
-        
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+       if (err)
+       { console.error(err); response.send("Error " + err); }
+        else
+         { response.render('pages/db', {results: result.rows} ); }
       });
-      
     });
+  });
 
 //**************************************************************************
 //***** mongodb get all of the Routes in Routes collection where frequence>=1
